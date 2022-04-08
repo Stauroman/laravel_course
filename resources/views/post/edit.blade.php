9e6b4d714a27dd98b47513c9201fa7345a1f5bfc
a.nikolaev@biopicmedical.com <HDK-Vmy-Dnr-23J>
2022-04-08 12:27:50 +0700

    Add categories and tags. add route::resources

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
     <div>
-        <form action="{{ route('post.update', $post->id)}}" method="POST">
+        <form action="{{ route('posts.update', $post->id)}}" method="POST">
             @csrf
             @method('patch')
             <div class="mb-3">
